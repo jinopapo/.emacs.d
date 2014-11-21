3a751b261eab82cf5adee9f0ef9bdc3474c88f9c
ji-no <mfm.ellegarden@gmail.com>
2014-11-21 16:18:44 +0900

    my emacs

--- inits/00_el-get.el
+++
@@ -1,6 +0,0 @@
-;;el-get
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get/")
-(setq el-get-dir "~/.emacs.d/el-get/el-get/")
-(require 'el-get)
-;; マイレシピの格納場所
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/el-get/recipes")
--- inits/08_shellpop.el
+++ inits/08_shellpop.el
@@ -1,6 +1,8 @@
 ; shell-pop の設定
 (require 'shell-pop)
 (global-set-key [f8] 'shell-pop)
-(shell-pop-set-internal-mode "ansi-term")
-(shell-pop-set-internal-mode-shell "/bin/zsh")
-(shell-pop-set-window-height 60)
+(custom-set-variables
+ '(shell-pop-shell-type (quote ("ansi-term" "*shell-pop-ansi-term*" (lambda nil (ansi-term shell-pop-term-shell)))))
+ '(shell-pop-term-shell "/bin/zsh")
+ '(shell-pop-window-height 50)
+ '(shell-pop-window-position "bottom"))
