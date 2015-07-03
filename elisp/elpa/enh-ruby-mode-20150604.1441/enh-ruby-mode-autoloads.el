eefbedf5179ff214a629408e592bc94850a6a1b6
ji-no <mfm.ellegarden@gmail.com>
2015-07-03 17:30:05 +0900

    add ruby-mode

---
+++ elisp/elpa/enh-ruby-mode-20150604.1441/enh-ruby-mode-autoloads.el
@@ -0,0 +1,31 @@
+;;; enh-ruby-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (enh-ruby-mode) "enh-ruby-mode" "enh-ruby-mode.el"
+;;;;;;  (21910 16446 298566 664000))
+;;; Generated autoloads from enh-ruby-mode.el
+
+(autoload 'enh-ruby-mode "enh-ruby-mode" "\
+Enhanced Major mode for editing Ruby code.
+
+\\{enh-ruby-mode-map}
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("enh-ruby-mode-pkg.el") (21910 16446 324366
+;;;;;;  870000))
+
+;;;***
+
+(provide 'enh-ruby-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; enh-ruby-mode-autoloads.el ends here
---
+++ inits/13_ruby-mode.el
@@ -0,0 +1,19 @@
+(require 'anything-rdefs)
+(autoload 'ruby-mode "ruby-mode"
+  "Mode for editing ruby source files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$latex " . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+	     (define-key ruby-mode-map "M-C-p" 'previous-multiframe-window)
+             (define-key ruby-mode-map "M-C-n" 'next-multiframe-window)
+	     (define-key ruby-mode (kbd "M-m") 'anything-rdefs)))
+;;ruby-electric
+(require 'ruby-electric)
+(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
+(setq ruby-electric-expand-delimiters-list nil)
+;; ruby-block.el --- highlight matching block
+(require 'ruby-block)
+(ruby-block-mode t)
+(setq ruby-block-highlight-toggle t)
