77f62320395caa1cf620e50c5117bd669c74fc4f
nezawa atsuhiro <ji-no@nezawa-no-MacBook-Air.local>
2015-11-23 23:07:26 +0900

    fix js-mode indet

--- inits/17_js2-mode.el
+++ inits/17_js2-mode.el
@@ -1,5 +1,4 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (add-hook 'js2-mode-hook
-	  '(lambda()
-	     (setq indent-tabs-mode nil)
-	     (setq web-mode-markup-indent-offset 2)))
+          '(lambda ()
+             (setq js2-basic-offset 2)))
