d5586869206bb41b98c5774818863a30225ef8fd
nezawa atsuhiro <mfm.ellegarden@gmail.com>
2016-04-24 00:01:06 +0900

    fix

--- inits/07_popwin.el
+++ inits/07_popwin.el
@@ -5,7 +5,6 @@
   (setq display-buffer-function 'popwin:display-buffer)
   (push '("anything" :regexp t :height 0.5) popwin:special-display-config)
   (push '("*magit" :regexp t :height 0.5) popwin:special-display-config)
-  (push '("COMMIT_EDITMSG" :height 0.5) popwin:special-display-config)
   (push '("*Completions*" :height 0.4) popwin:special-display-config)
   (push '("*compilation*" :height 0.4 :noselect t :stick t) popwin:special-display-config)
   (push '(direx:direx-mode :position left :width 25 :dedicated t)popwin:special-display-config)
