8a00b6ff7f6c4f6e238170518ed164db53b428ea
nezawa atsuhiro <mfm.ellegarden@gmail.com>
2016-04-23 17:56:44 +0900

    add helm

--- inits/16_web-mode.el
+++ inits/16_web-mode.el
@@ -1,4 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
 (add-hook 'web-mode-hook
 	  '(lambda()
 	     (setq web-mode-markup-indent-offset 2)))
---
+++ inits/20_helm.el
@@ -0,0 +1,5 @@
+(require 'helm-config)
+(helm-mode 1)
+(define-key global-map (kbd "C-x b") 'helm-mini)
+(define-key global-map (kbd "C-x C-f") 'helm-find-files)
+(define-key global-map (kbd "M-x") 'helm-M-x)
